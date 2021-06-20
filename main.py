fbf5c68190f551185040c128d5eccb76d1c79fa0
NDT2000 <64462883+NDT2000@users.noreply.github.com>
2021-06-20 21:37:49 +0530

    Final

---
+++ main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question_number in range(0, len(question_data)):
+    new_question = Question(question_data[question_number]["text"], question_data[question_number]["answer"])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz!!")
+print(f"Your total score is {quiz.score}/{quiz.question_number}")
+
+
